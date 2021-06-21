b79c06a914d791e8bf0583aa19a25b31e4855d8f
ganapathi-devop <ganapathy1red1@gmail.com>
2021-06-21 21:16:51 +0530

    last commit 21-6-21

--- script.js
+++ script.js
@@ -27,57 +27,27 @@ $(document).ready(function(){
         }
     });
    //scroll to top button function
-
-    // $(window).scroll(function(){
-    //     function scrollFunction(){
-    //         if(document.scrollTop > 20) {
-    //             $("#scrolltop").css("display" "block");
-    //         }else{
-    //             $("#scrolltop").css("display" "none");
-    //         }
-    //     }
-    //     $("#scrolltop").click(function(){
-    //         document.body.scrollTop = 0;
-    //         document.documentElement.scrollTop = 0;
-    //     });
-    // });
-
     //Get the button:
-mybutton = document.getElementById("myBtn");
+  mybutton = document.getElementById("myBtn");
 
-// When the user scrolls down 10px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+  // When the user scrolls down 10px from the top of the document, show the button
+  window.onscroll = function() {scrollFunction()};
 
-function scrollFunction() {
-  if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  $("#mybtn").css("transition" , "1s");
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
-
-//onclick to play video on video section
-let video = $("iframe");
-let cls = $(".video");
-let ply = $("#play");
-function videopop(){
-  ply.click(
-    function () {
-      video.removeClass(cls);
-      console.log("hello");
+  function scrollFunction() {
+    if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+      mybutton.style.display = "block";
+    } else {
+      mybutton.style.display = "none";
     }
-  )
-  ply.click(function(){
-    $(this).hide();
-  });
-}
+  }
+
+  // When the user clicks on the button, scroll to the top of the document
+  function topFunction() {
+    $("#mybtn").css("transition" , "1s");
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  }
+//----end of document ready function-----
 });
 
 
