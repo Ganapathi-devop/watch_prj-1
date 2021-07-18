f03cc7701cd741e8760fea91f25a366456fea36c
ganapathi-devop <ganapathy1red1@gmail.com>
2021-07-18 14:00:42 +0530

    commit on 18-7-21

--- script/script.js
+++ script/script.js
@@ -4,11 +4,8 @@ $(document).ready(function(){
     $("#navbar-bar").click(function(){
         if( i % 2 === 0) {
             $("#navbar-list").css("display", "block");
-            i++
-            console.log(i)
         }else{
             $("#navbar-list").css("display", "none");
-            console.log(i)
             i++
         }
     });
@@ -20,39 +17,30 @@ $(document).ready(function(){
         if(window.pageYOffset > 150){
             banner.addClass("content")
             header.addClass("sticky");
-            console.log("if");
         }else{
             banner.removeClass("content")
             header.removeClass("sticky");
-            console.log("else");
        }
     });
    //scroll to top button function
     //Getting the button:
-  mybutton = document.getElementById("scrollbtn");
+  mybutton = $("#scrollbtn");
 
   // When the user scrolls down 10px from the top of the document, show the button
-  window.onscroll = function() {scrollFunction()};
-
-  function scrollFunction() {
-    if (document.body.scrollTop > 15 || document.documentElement.scrollTop > 15 ) {
-      mybutton.style.display = "block";
-    } else {
-      mybutton.style.display = "none";
-    }
-  }
+  $(function () {
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 100) {
+            mybutton.css("display", "block"); // show the button
+        } else {
+          mybutton.css("display", "none"); // hide the button
+        }
+    });
+  });
 
   // When the user clicks on the button, scroll to the top of the document
-  $("#scrollbtn").click( function topFunction() {
-    var doc = document.documentElement;
-    doc.animate({scrollTop = 0}, "slow");
-  } );
-
-  //on click funtion for plybtn in video section
-  $(".player").click( ()=>{
-    var video_div = document.createElement("div");
-    var doc = document.getElementsByName("body");
-    doc.appendChild(video_div);
+  mybutton.click( function() {
+    $("html", "body").animate({ scrollTo: 0}, 800);
+    console.log("going top");
   } );
 
 // index section ends
